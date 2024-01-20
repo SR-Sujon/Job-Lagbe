397511b5751d286cb2c438e8cbdbe8a69fcaf847
Saidur Rahman Sujon <sr.sujon.cyb@gmail.com>
2024-01-20 15:53:21 +0600

    Navbar Structure updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 import Wrapper from "../assets/wrappers/Navbar";
-
+import { FaAlignLeft } from "react-icons/fa";
+import Logo from "./Logo";
+import { useDashboardContext } from "../pages/DashboardLayout";
+// [React Icons](https://react-icons.github.io/react-icons/)
 const Navbar = () => {
-  return <Wrapper>Navbar</Wrapper>;
+  const { toggleSidebar } = useDashboardContext();
+  return (
+    <Wrapper>
+      <div className="nav-center">
+        <button type="button" className="toggle-btn" onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className="logo-text">dashboard</h4>
+        </div>
+        <div className="btn-container">toggle/logout</div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Navbar;
