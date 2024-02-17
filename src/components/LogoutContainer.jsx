2b289fc79912c4d55b3e7a4e5a33d67f19539908
Saidur Rahman Sujon <sr.sujon.cyb@gmail.com>
2024-02-17 22:39:38 +0600

    Big Sidebar Updated

---
+++ src/components/LogoutContainer.jsx
@@ -0,0 +1,22 @@
+import { FaUserCircle, FaCaretDown } from "react-icons/fa";
+import Wrapper from "../assets/wrappers/LogoutContainer";
+import { useState } from "react";
+import { useDashboardContext } from "../pages/DashboardLayout";
+
+const LogoutContainer = () => {
+  const [showLogout, setShowLogout] = useState(false);
+  const { user, logoutUser } = useDashboardContext();
+  return (
+    <Wrapper>
+      <button
+        type="button"
+        className="btn logout-btn"
+        onClick={() => setShowLogout(!showLogout)}
+      >
+        
+      </button>
+    </Wrapper>
+  );
+};
+
+export default LogoutContainer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import Wrapper from "../assets/wrappers/Navbar";
 import { FaAlignLeft } from "react-icons/fa";
 import Logo from "./Logo";
 import { useDashboardContext } from "../pages/DashboardLayout";
-// [React Icons](https://react-icons.github.io/react-icons/)
+import { LogoutContainer } from "../components";
+
 const Navbar = () => {
   const { toggleSidebar } = useDashboardContext();
   return (
@@ -15,7 +16,9 @@ const Navbar = () => {
           <Logo />
           <h4 className="logo-text">dashboard</h4>
         </div>
-        <div className="btn-container">toggle/logout</div>
+        <div className="btn-container">
+          <LogoutContainer />
+        </div>
       </div>
     </Wrapper>
   );
